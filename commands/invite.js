880eedeb7ab5316e4c757348c6495398f00a6f82
OnlyDK <87594508+OnlyDK01@users.noreply.github.com>
2021-08-25 14:50:57 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -13,7 +13,7 @@ module.exports.run = async (client, message, args) => {
 const privado = new Discord.MessageEmbed()
   .setTitle(`Invite:`)
   .setColor("PURPLE")
-  .setDescription("<:Arrow1:865837531240726538>Aqui está o meus links de convite:\n•[Convite com todas as permissoês necessarias](https://discord.com/oauth2/authorize?client_id=875766257197129758&scope=bot&permissions=388160)\n•[Convite com permissão de administrador](https://discord.com/oauth2/authorize?client_id=875766257197129758&scope=bot&permissions=8)\n\nCaso queira acessar meu site: [Clique aqui](https://shinou.webnode.com)\nMeu servidor de suporte: [Clique aqui](https://discord.gg/pJm5PWZmRa)\n\nDesenvolvida por: <@437793544141406209>")
+  .setDescription("...")
   .setFooter(`Executado por ${message.author.tag}`, message.author.displayAvatarURL())
   .setThumbnail("https://media0.giphy.com/media/Yq1tXTNe5PDdQEhKXG/200.gif")
   message.author.send(privado);
